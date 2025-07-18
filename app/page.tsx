1ef80e489c867b743a87210a5373e78376aa900c
Sharvesh Srinivasan <sharevsh308@gmail.com>
2025-07-18 17:59:23 +0530

    2nd commit

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,8 @@ export default function CarPricePrediction() {
     setError(null);
   
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      const response = await fetch(`${apiUrl}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
